fd9ccd56ae7c64615763055166c8cda375615a83
Lokrogamer <lokrogamer.29@gmail.com>
2026-06-05 12:20:00 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -50,60 +50,17 @@ export default function Home() {
       <section className="my-10 mx-3 flex flex-col items-center">
        <p className="my-7 text-2xl font-bold text-black">Our Services</p>
 <div className="grid gris-cols-1 gap-6">
-      <div className="max-w-75 border border-black rounded-xl p-5 bg-black transition-transform duration-300 ease-in-out hover:scale-103 hover:-translate-y-1 "> 
+      <div className="w-[85%] border border-black rounded-xl p-5 bg-black transition-transform duration-300 ease-in-out hover:scale-103 hover:-translate-y-1 "> 
 
         <div className="p-3 my-2">
-<p className="text-yellow-300 border-b border-grey-300 text-2xl font-bold hover:border-white transition-transform duration-300 ease-in-out p-5">Communication & Sales</p>
+<p className="text-yellow-300 border-b border-grey-300 text-2xl font-bold hover:border-white transition-transform duration-300 ease-in-out p-5">Service Title</p>
           </div>
        <ul className="list-disc flex flex-col mx-5 my-7 text-white gap-5">
 
-<li className="mt-5 mb-3"><p className="text-lg font-bold"> 1stLine of Engagement</p></li>
-
-<li>To engage exhibitors as soon as possible</li>
-<li>To confirm exhibitors’ participation</li>
-<li>To provide exhibitor’s manual</li>
-<li>To provide hall & booth space layout</li>
-<li>To confirm type of booth construction</li>
-<li>To process deposits for booth space booking</li>
-
-<li className="mt-5 mb-3"><p className="text-lg font-bold">2nd Line of Engagement</p></li>
-
-<li>To promote Design & Build Services</li>
-<li>To assist with taking of design brief</li>
-<li>To assist with exhibition</li>
-<li>To confirm type of booth constructionrelated matters</li>
-<li>To assist with any other requirements like travel</li>
-        
+<li><p className="text—xl font-bold text-black">Add your service here</p></li>
          </ul>
        </div>
-<div className="max-w-75 border border-black rounded-xl p-5 bg-black transition-transform duration-300 ease-in-out hover:scale-103 hover:-translate-y-1 ">
 
-        <div className="p-3 my-2">
-<p className="text-yellow-300 border-b border-grey-300 text-2xl font-bold hover:border-white transition-transform duration-300 ease-in-out p-5">Design of the country pavilion</p>
-          </div>
-       <ul className="list-disc flex flex-col mx-5 my-7 text-white gap-5">
-          <li className="mt-5 mb-3"><p className="text-lg font-bold">Raw Space Design</p></li>
-<li>Conceptualization & Design</li>
-<li>Content Creations</li>
-<li>3D Renderings</li>
-<li>2D Sectional drawings</li>
-<li>Technical Design and Technical Planning</li>
-<li>Urban/ Rural Design</li>
-<li>Service Design</li>
-<li>Human - centered Design</li>
-<li>Furniture Design</li>
-<li>Digital Design</li>
-<li>Content Design and Content Making</li>
-
-<li className="mt-5 mb-3"><p className="text-lg font-bold">Graphic Design</p></li>
-<li>Visual Content Creations</li>
-<li>Design Layout</li>
-<li>Used in catalogues, brochures, marketing flyers, pull-up banners, etc.</li>
-        </ul>
-       </div>
-
-
-</div>
       </section>
     </>
   );
