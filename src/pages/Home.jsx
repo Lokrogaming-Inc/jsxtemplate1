b510e013e712960dd59cb9d36348e036515aca10
Lokrogamer <lokrogamer.29@gmail.com>
2026-06-05 12:23:30 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ export default function Home() {
       
       <section className="py-20">
         <div className="max-w-7xl mx-auto px-6">
-<p className="text-center mb-4 text-2xl text-black font-bold border-b border-black">People Who Trust Us</p>
+<p className="text-center mb-4 text-2xl text-white font-bold border-b border-black">People Who Trust Us</p>
           <div className="grid md:grid-cols-3 gap-6">
 
             <div className="bg-slate-800 p-6 rounded-2xl">
@@ -48,16 +48,16 @@ export default function Home() {
       </section>
        <Divider />
       <section className="my-10 mx-3 flex flex-col items-center">
-       <p className="my-7 text-2xl font-bold text-black">Our Services</p>
+       <p className="my-7 text-2xl font-bold text-white">Our Services</p>
 <div className="grid gris-cols-1 gap-6">
       <div className="w-[85%] border border-black rounded-xl p-5 bg-black transition-transform duration-300 ease-in-out hover:scale-103 hover:-translate-y-1 "> 
 
         <div className="p-3 my-2">
 <p className="text-yellow-300 border-b border-grey-300 text-2xl font-bold hover:border-white transition-transform duration-300 ease-in-out p-5">Service Title</p>
           </div>
-       <ul className="list-disc flex flex-col mx-5 my-7 text-white gap-5">
+       <ul className="list-none flex flex-col mx-5 my-7 text-white gap-5">
 
-<li><p className="text—xl font-bold text-black">Add your service here</p></li>
+<li><p className="text—xl font-bold text-white">Add your service here</p></li>
          </ul>
        </div>
 </div>
